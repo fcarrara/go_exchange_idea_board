fa130498e182de318f079f98033e84094394d5fd
Fabricio Carrara <fabricio@omise.co>
2019-10-22 03:46:03 +0700

    [Fabricio] Action items index page

--- lib/go_exchange_idea_board_web/live/action_item/index.ex
+++ lib/go_exchange_idea_board_web/live/action_item/index.ex
@@ -3,7 +3,7 @@ defmodule GoExchangeIdeaBoardWeb.ActionItemLive.Index do
 
   alias GoExchangeIdeaBoard.EventCenter
   alias GoExchangeIdeaBoard.Retrospectives.ActionItems
-  alias GoExchangeIdeaBoardWeb.ActionLiveView
+  alias GoExchangeIdeaBoardWeb.ActionItemView
 
   def mount(_session, socket) do
     EventCenter.subscribe()
@@ -11,13 +11,13 @@ defmodule GoExchangeIdeaBoardWeb.ActionItemLive.Index do
     {:ok, fetch(socket)}
   end
 
-  def render(assigns), do: ActionLiveView.render("index.html", assigns)
+  def render(assigns), do: ActionItemView.render("index.html", assigns)
 
   defp fetch(socket) do
     assign(socket, action_items: ActionItems.list_action_items())
   end
 
-  def handle_event("delete_action_item", id, socket) do
+  def handle_event("delete_action_item", %{"id" => id}, socket) do
     action_item = ActionItems.get_action_item!(id)
     {:ok, _action_item} = ActionItems.delete_action_item(action_item)
 
