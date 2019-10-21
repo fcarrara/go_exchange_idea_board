825c2d8447d435f634741ecc72e49e97407d64c7
Fabricio Carrara <fabricio@omise.co>
2019-10-22 03:51:53 +0700

    [Fabricio] Adsd helpers in retro session view

--- lib/go_exchange_idea_board_web/views/retro_session_view.ex
+++ lib/go_exchange_idea_board_web/views/retro_session_view.ex
@@ -1,8 +1,6 @@
 defmodule GoExchangeIdeaBoardWeb.RetroSessionView do
   use GoExchangeIdeaBoardWeb, :view
 
-  alias GoExchangeIdeaBoardWeb.{NoteLive, RetroSessionLive}
-
   def get_note_value(changeset) do
     {:ok, value} = Map.fetch(changeset.data, :content)
 
@@ -13,5 +11,15 @@ defmodule GoExchangeIdeaBoardWeb.RetroSessionView do
     Timex.format!(date, "{D}-" <> Timex.month_shortname(date.month) <> "-{YYYY}")
 
     # Timex.format!(date, Timex.day_shortname(Date.day_of_week(date)) <> " {D}-" <> Timex.month_shortname(date.month) <> "-{YYYY}")
+  end
+
+  def get_grouped_notes(notes) do
+    notes
+    |> Enum.group_by(& &1.group_id)
+  end
+
+  def get_total_votes(notes) do
+    notes
+    |> Enum.reduce(0, fn note, acc -> note.votes + acc end)
   end
 end
