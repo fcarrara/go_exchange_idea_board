8f2337ef00acead1eab2e6eb87ad4ddfa684a1e7
Fabricio Carrara <fabricio@omise.co>
2019-10-22 03:48:49 +0700

    [Fabricio] Improve main layout

--- lib/go_exchange_idea_board_web/views/layout_view.ex
+++ lib/go_exchange_idea_board_web/views/layout_view.ex
@@ -1,8 +1,6 @@
 defmodule GoExchangeIdeaBoardWeb.LayoutView do
   use GoExchangeIdeaBoardWeb, :view
 
-  alias GoExchangeIdeaBoardWeb.{RetroFormatLive, RetroSessionLive, ActionItemLive}
-
   def is_active_menu?(conn, module) do
     if controller_module(conn) == module, do: "active", else: ""
   end
